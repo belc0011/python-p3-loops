b3d70c77c84024a3ab47ce9ce7a82ee388f99f5d
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-20 18:16:56 -0700

    complete loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(11, 0, -1):
+        if i != 1:
+            print(i - 1)
+            i -= 1
+        else:
+            print("Happy New Year!")
+            i -= 1
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = [int * int for int in int_list]
+    return squared_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101, 1):
+        if i == 1:
+            print(i)
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+           
+
